ac051443dd1f5b05fa968c58d3472d0517f2489a
PromiseSyl <104115299+Program-doctor@users.noreply.github.com>
2023-01-20 03:09:16 +0100

    Aligned the forecast to the center
    Added more responsiveness to the layout

--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -14,12 +14,12 @@ export default function Weatherinfo(props){
     <div className="col-6">
       <div className="imgtemp">
     <img src={props.data.icon} alt={props.data.desc}/>
-    <h2><span>{Math.round(props.data.temp)}</span><span className="unit">&deg;C</span></h2>
+    <h2><span>{Math.round(props.data.temp)}</span><span className="unit">&deg;C<a href="/" className="link fahrenheit">|&deg;F</a></span></h2>
       </div>
     </div>
     <div className="col-6 info">
-    <p>Humidity:{Math.round(props.data.humid)}%</p>
-    <p>Wind:{Math.round(props.data.wind)}km /h</p>
+    <p>Humidity: {Math.round(props.data.humid)}%</p>
+    <p>Wind: {Math.round(props.data.wind)}km/h</p>
     </div>
     </div>
     </div>);
